6e3d3ad636f42dfc6902ff410b03108bfc86c2f9
AdrianVyne <imadrian1234@gmail.com>
2023-02-19 21:57:46 +0800

    updated news html with animations and polishing

---
+++ JS/sports.js
@@ -0,0 +1,36 @@
+const hamburgerSection = document.querySelector(".hamburger-section");
+const navBarMobile = document.querySelector(".nav-bar-mobile");
+
+hamburgerSection.addEventListener("click", function () {
+  navBarMobile.classList.toggle("show");
+});
+
+const toggles = document.querySelectorAll(".faq-toggle");
+
+toggles.forEach((toggle) => {
+  toggle.addEventListener("click", () => {
+    toggle.parentNode.classList.toggle("active");
+  });
+});
+
+// Toggle function for each dropdown menu
+function toggleDropdown(dropdown) {
+  const dropdownSection = document.querySelector(`.${dropdown}`);
+  const dropdownToggle = dropdownSection.querySelector(".toggle-link");
+
+  dropdownToggle.addEventListener("click", () => {
+    dropdownSection.classList.toggle("active");
+    dropdownToggle.classList.toggle("active");
+  });
+}
+
+// Call the toggle function for each dropdown menu
+toggleDropdown("academic");
+toggleDropdown("extracurricularActivities");
+toggleDropdown("resources");
+
+// nav button
+const toggle = document.getElementById("toggle");
+const nav = document.getElementById("nav");
+
+toggle.addEventListener("click", () => toggle.classList.toggle("active"));
